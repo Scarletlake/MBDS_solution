32ef7ef0b48220897272828a643a8250b0af5bf1
Hui OUYNAGH <ouyangh1@student.unimelb.edu.au>
2021-02-24 11:07:12 +0800

    update q6

--- Solution/Question 6/Q6.py
+++ Solution/Question 6/Q6.py
@@ -37,26 +37,42 @@ def is_intersect(point, polygon_v1, polygon_v2):
     elif point[0] > polygon_v1[0] and point[0] >  polygon_v2[0]:
         return False
 
-    # The ray pass through one of or both the vertices
+    # The ray pass through one of or both the vertices, and it is below the edge
     elif ((point[1] == polygon_v1[1] and point[1] < polygon_v2[1]) or
           (point[1] == polygon_v2[1] and point[1] < polygon_v1[1])):
         return False
-
-    # The point of intersection is at the right of the ray
-    elif (point[0] < polygon_v1[0] and point[0] <  polygon_v2[0] and
-          (polygon_v1[1] < point[1] < polygon_v2[1] or polygon_v1[1] > point[1] > polygon_v2[1]) ):
+    
+    # The ray pass through one of or both the vertices, and it is above the edge
+    elif ((point[1] == polygon_v1[1] and point[0] <= polygon_v1[0] and point[1] > polygon_v2[1]) or
+          (point[1] == polygon_v2[1] and point[0] <= polygon_v2[0] and point[1] > polygon_v1[1])):        
+        return True
+    
+    # The ray is at the left of the edge and the point of intersection is at the right of the ray
+    elif (point[0] <= polygon_v1[0] and point[0] <=  polygon_v2[0] and
+          (polygon_v1[1] < point[1] < polygon_v2[1] or polygon_v1[1] > point[1] > polygon_v2[1]) ):        
         return True
     
     # The point of intersection is at the left of the ray
     # Consider the edge as a function, if the point is above the
     # line, then the ray does not intersect with the edge.
     # slop = (v1_x-v2_x)/(v1_y-v2_y)
-    elif ((polygon_v1[0]-polygon_v2[0])/(polygon_v1[1]-polygon_v2[1])*(point[0]-polygon_v1[0]) >
-          (point[1]-polygon_v1[1])):
-        return False
+    
+    
+    slop = (polygon_v1[0] - polygon_v2[0]) / (polygon_v1[1] - polygon_v2[1])
+    start_v = polygon_v1
+    end_v = polygon_v2
+    if polygon_v1[0] > polygon_v1[1]:
+        start_v = polygon_v2
+        end_v = polygon_v1
+        
+    if slop *(point[0]-start_v[0]) > (point[1]-start_v[1]): # below the line
+        if slop < 0:    
+            return False
+        else:
+            return True
 
-    else:
-        return True
+
+        
 
 
 
@@ -68,14 +84,15 @@ def test_location(points, polygon, output_file_name):
     # Test each point
     for point in points:
         intersect_num = 0
-        for i in range(len(polygon)-1):
+        for i in range(len(polygon)-1):           
+    
             if is_intersect(point, polygon[i], polygon[i+1]):
                 intersect_num += 1
         
 
         # Check if the number of intersection is odd number or even number
         # if odd, then inside, otherwise outside
-        f.write(str(point[0]) + " " + str(point[1]) + " ")
+        f.write(str(point[0]) + " " + str(point[1]) + " " + str(intersect_num)+ " ")
         if intersect_num % 2 == 1:
             f.write("inside\n")
         else:
